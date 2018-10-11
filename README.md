38954510b1d14729448f7864e8f5da63eba10b9c
Shubin Mai <isaac@ShubindeMacBook-Air.local>
2018-10-11 13:10:59 +0800

    add ad func.

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 python wifi helper in raspberry pi
 
 # how to use
+
+STEP 0, https://gist.github.com/Lewiscowles1986/fecd4de0b45b2029c390#gistcomment-1716402
+```
+sudo mkdir -p /etc/pi-ap
+sudo cp /etc/network/interfaces /etc/pi-ap/standard-config
+wget https://gist.github.com/Lewiscowles1986/fecd4de0b45b2029c390/raw/0c8b3af3530a35db9ab958defe9629cb5ea99972/rPi3-ap-setup.sh
+chmod +x rPi3-ap-setup.sh
+sudo apt-get update
+sudo ./rPi3-ap-setup.sh password apname
+sudo cp /etc/network/interfaces /etc/pi-ap/ap-mode-config
+```
+
 STEP 1    
 ```
 cd /etc/wpa_supplicant/; cp wpa_supplicant.conf wpa_supplicant.conf.bak  
--- pifi.py
+++ pifi.py
@@ -1,5 +1,7 @@
-#coding=utf-8
+# coding=utf-8
 import subprocess
+
+
 class WifiHelper(object):
 
     @staticmethod
@@ -9,11 +11,33 @@ class WifiHelper(object):
     @staticmethod
     def add_ssid(ssid, password):
         WifiHelper.clean_ssid()
-        for s in ["network={", r'ssid="%s"'%(ssid), r'psk="%s"'%(password), "}"]:
-            subprocess.call("echo '%s' | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf"%(s), shell=True)
+        for s in ["network={",
+                  r'ssid="%s"' % (ssid),
+                  r'psk="%s"' % (password),
+                  "}"]:
+            subprocess.call("echo '%s' | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf" % (s), shell=True)
         subprocess.call("/sbin/wpa_cli -i wlan0 reconfigure", shell=True)
 
     @staticmethod
     def clean_ssid():
-        subprocess.call("sudo cp /etc/wpa_supplicant/wpa_supplicant.conf.bak /etc/wpa_supplicant/wpa_supplicant.conf", shell=True)
+        subprocess.call("sudo cp /etc/wpa_supplicant/wpa_supplicant.conf.bak /etc/wpa_supplicant/wpa_supplicant.conf",
+                        shell=True)
         subprocess.call("/sbin/wpa_cli -i wlan0 reconfigure", shell=True)
+
+    @staticmethod
+    def start_ap():
+        for cmd in ["sudo update-rc.d hostpad enable",
+                    "sudo update-rc.d dnsmasq enable",
+                    "sudo rm /etc/network/interfaces",
+                    "sudo ln -f /etc/pi-ap/ap-mode-config /etc/network/interfaces",
+                    "sudo shutdown -r 0"]:
+            subprocess.call(cmd, shell=True)
+
+    @staticmethod
+    def stop_ap():
+        for cmd in ["sudo update-rc.d hostpad disable",
+                    "sudo update-rc.d dnsmasq disable",
+                    "sudo rm /etc/network/interfaces",
+                    "sudo ln -f /etc/pi-ap/standard-config /etc/network/interfaces",
+                    "sudo shutdown -r 0"]:
+            subprocess.call(cmd, shell=True)
