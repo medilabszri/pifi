26df69786247b8e0f5d1d1640d980f0808c0f9b5
medilabszri <43983079+medilabszri@users.noreply.github.com>
2018-10-16 11:23:54 +0800

    Update change_source.sh

--- change_source.sh
+++ change_source.sh
@@ -1,5 +1,3 @@
-sudo su
-
 cat > /etc/apt/sources.list <<EOF
 deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ jessie main contrib non-free rpi
 deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ jessie main contrib non-free rpi
@@ -9,3 +7,5 @@ cat > /etc/apt/sources.list.d/raspi.list <<EOF
 deb http://mirror.tuna.tsinghua.edu.cn/raspberrypi/ jessie main ui
 deb-src http://mirror.tuna.tsinghua.edu.cn/raspberrypi/ jessie main ui
 EOF
+
+apt-get update
