fd8cc7975b8bd5f3d7a40bcf3252b1d0f84294f3
Shubin Mai <isaac@ShubindeMacBook-Air.local>
2018-10-18 11:11:04 +0800

    add threading loop.

--- pifi.py
+++ pifi.py
@@ -1,12 +1,31 @@
 # coding=utf-8
 import subprocess
+import threading, time
 
+abs_file=__file__
+print("abs path is %s" %(__file__))
+abs_dir=abs_file[:abs_file.rfind("/")]     # windows下用\\分隔路径，linux下用/分隔路径
+print abs_dir
 
 class WifiHelper(object):
 
     @staticmethod
+    def get_wlan_macaddress():
+        hostName= subprocess.check_output("hostname", shell=True)
+        s = ''
+        if hostName == "ShubindeMacBook-Air.local\n":
+            s = subprocess.check_output("/sbin/ifconfig en0 | grep ether", shell=True)
+            s1 = s[s.find('ether'):]
+        else:
+            s = subprocess.check_output("/sbin/ifconfig wlan0 | grep HW", shell=True)
+            s1 = s[s.find('HW'):]
+        result= s1.split(' ')[1].replace(':', '')
+        print result
+        return result
+
+    @staticmethod
     def check_wlan0():
-        return 'broadcast' in subprocess.check_output(['/sbin/ifconfig', 'wlan0'])
+        return 'Bcast' in subprocess.check_output(['/sbin/ifconfig', 'wlan0'])
 
     @staticmethod
     def add_ssid(ssid, password):
@@ -16,28 +35,66 @@ class WifiHelper(object):
                   r'psk="%s"' % (password),
                   "}"]:
             subprocess.call("echo '%s' | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf" % (s), shell=True)
-        subprocess.call("/sbin/wpa_cli -i wlan0 reconfigure", shell=True)
+        subprocess.call("sudo /sbin/wpa_cli -i wlan0 reconfigure", shell=True)
 
     @staticmethod
     def clean_ssid():
         subprocess.call("sudo cp /etc/wpa_supplicant/wpa_supplicant.conf.bak /etc/wpa_supplicant/wpa_supplicant.conf",
                         shell=True)
-        subprocess.call("/sbin/wpa_cli -i wlan0 reconfigure", shell=True)
+        subprocess.call("sudo /sbin/wpa_cli -i wlan0 reconfigure", shell=True)
 
     @staticmethod
     def start_ap():
-        for cmd in ["sudo update-rc.d hostpad enable",
-                    "sudo update-rc.d dnsmasq enable",
-                    "sudo rm /etc/network/interfaces",
-                    "sudo ln -f /etc/pi-ap/ap-mode-config /etc/network/interfaces",
+        print("starting ap...")
+        for cmd in ["sudo bash %s/start_ap.sh"%(abs_dir),
                     "sudo shutdown -r 0"]:
             subprocess.call(cmd, shell=True)
 
     @staticmethod
     def stop_ap():
-        for cmd in ["sudo update-rc.d hostpad disable",
-                    "sudo update-rc.d dnsmasq disable",
-                    "sudo rm /etc/network/interfaces",
-                    "sudo ln -f /etc/pi-ap/standard-config /etc/network/interfaces",
+        print("stoping ap...")
+        for cmd in ["sudo bash %s/stop_ap.sh"%(abs_dir),
                     "sudo shutdown -r 0"]:
             subprocess.call(cmd, shell=True)
+
+    def __init__(self):
+        self.isconnect = False
+        self.time_interval = 5
+
+    def work(self):
+        """
+        周期性检查网络连接状态
+        """
+        self.working = True
+        while self.working:
+            self.isconnect = self.check_wlan0()
+            if self.isconnect:
+                print "wlan0 is connected."
+                oh = self.on_wifi_available_callback
+                if oh:
+                    oh()
+            else:
+                print "wlan0 is disconnected."
+                oh = self.on_wifi_disconnect_callback
+                if oh:
+                    oh()
+            time.sleep(self.time_interval)
+
+    def spin(self):
+        self.t = threading.Thread(target=self.work)
+        self.t.start()
+
+    def join(self):
+        self.t.join()
+
+    def set_callback(self, on_wifi_disconnect_callback=None, on_wifi_available_callback=None):
+        """
+        :param on_wifi_disconnect_callback: 如其名
+        :param on_wifi_available_callback: 如其名
+        """
+        self.on_wifi_disconnect_callback = on_wifi_disconnect_callback
+        self.on_wifi_available_callback = on_wifi_available_callback
+
+    def stop(self):
+        self.working = False
+
